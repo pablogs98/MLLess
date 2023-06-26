f0bf8a575204243247d326299657abff107b7d64
Pablo Gimeno <pablo.gimeno@urv.cat>
2023-06-26 10:54:36 +0200

    Update matrix_factorisation.py

--- examples/pytorch/matrix_factorisation.py
+++ examples/pytorch/matrix_factorisation.py
@@ -23,14 +23,14 @@ if __name__ == '__main__':
     threshold = [0.74, 0.76]
 
     n_features = 20
-    epochs = 1
+    epochs = 20
     repetitions = 1
     learning_rate = 8.0
 
-    dataset_remote_bucket = "bucket"
+    dataset_remote_bucket = "pablo-data"
 
     for j in range(len(n_users)):
-        execution_name = f"{dataset_name[j].replace('/', '')}-mf-{n_workers}-workers-rep{i}"
+        execution_name = f"{dataset_name[j].replace('/', '')}-mf-{n_workers}-workers"
         execution = MatrixFactorisation(n_users[j], n_items[j], n_features, threshold[j], epochs, execution_name,
                                         learning_rate, num_minibatches[j], dataset_name[j], dataset_remote_bucket)
         run(n_workers, execution, init_rank, end_rank, address, port)
